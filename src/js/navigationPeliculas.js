87482f72070a03e1280c25df068f75c13fbfba31
Juan David Prieto Velasquez <david2005prieto@gmail.com>
2024-04-04 15:02:16 -0500

    V-2 Se creo un archivo js de la navegacion de las secciones y se le asignaron funciones a los botones de la navbar

--- src/js/navigationPeliculas.js
+++ src/js/navigationPeliculas.js
@@ -1,16 +1,42 @@
+// Este eventlistener se ejecutara funciones al cambiar de location hash
+window.addEventListener("hashchange", navigatorPeliculas, false);
+
+// Mientras que este se cambiara apenas el contenido de la pagina ya haya cargado
+window.addEventListener("DOMContentLoaded", navigatorPeliculas, false);
+
 function navigatorPeliculas() {
-  if (location.hash.startsWith("#trends")) {
-    console.log("Trends");
+  // Dependiendo del location hash se ejecutaran las siguiente funciones
+  if (location.hash.startsWith("#dailyTrends")) {
+    dailyTrendsPage();
+  } else if (location.hash.startsWith("#weeklyTrends")) {
+    weeklyTrendsPage();
   } else if (location.hash.startsWith("#search=")) {
-    console.log("Search!");
+    searchPage();
   } else if (location.hash.startsWith("#movie=")) {
-    console.log("Movie!");
-  } else if (location.hash.startsWith("#category=")) {
-    console.log("Category!");
+    moviesPage();
+  } else if (location.hash.startsWith("#favorites")) {
+    favoritesPage();
   } else {
-    console.log("Home");
+    homePage();
   }
 }
 
-window.addEventListener("hashchange", navigatorPeliculas, false);
-window.addEventListener("DOMContentLoaded", navigatorpeliculas), false;
+function dailyTrendsPage() {
+  console.log("Trends");
+}
+
+function weeklyTrendsPage() {}
+
+function searchPage() {
+  console.log("Search!");
+}
+
+function moviesPage() {
+  console.log("Movie!");
+}
+
+function favoritesPage() {
+  console.log("Category!");
+}
+
+function homePage() {}
